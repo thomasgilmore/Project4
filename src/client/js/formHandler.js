cf4c575569b6f9ba7d7c7b273e653ae7f602eb69
Thomas Gilmore <tomtitans@aol.com>
2019-12-03 13:28:35 -0500

    refactor: Coding fetch of website after submit is clicked
    - Adding code to what should happen after user clicks submit on web app.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,42 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    //Get input from form
+    var url = document.querySelectorAll('input[name=test-url]')
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    //check that input is a valid url
+    if(Client.validURL(JSON.parse(JSON.stringify(url[0].value))))
+    {
+        console.log("::: Form Submitted :::")
+        
+        console.log("BUILDING REQUEST");
+        console.log(JSON.stringify(url[0].value));
+        fetch('http://localhost:3000/article', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({text: url[0].value})
+        })
+        .then(res => res.json())
+        .then(function(res) {
+            console.log(res); // for debugging
+            document.querySelector('section.url-results #polarity').innerHTML = res.polarity
+            document.querySelector('section.url-results #subjectivity').innerHTML = res.subjectivity
+            document.querySelector('section.url-results #polarity_confidence').innerHTML = res.polarity_confidence
+            document.querySelector('section.url-results #subjectivity_confidence').innerHTML = res.subjectivity_confidence
+            document.querySelector('section.url-results #excerpt').innerHTML = res.text
+        })
+
+    }else{
+        // Display error message if URL is not valide
+        var error_sec = document.querySelector('section.errors');
+        var error = document.querySelector('section.errors #error');
+        error.innerHTML = "URL:\"" +JSON.stringify(url[0].value)+"\" is not valide. Please enter a valid url"
+        error_sec.style.display = "block";
+        
+    } 
 }
 
-export { handleSubmit }
+module.exports = handleSubmit;
