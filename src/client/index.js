a4bab0cd7bd55ebd3f2d68066276e88f0c0cefd0
Thomas Gilmore <tomtitans@aol.com>
2019-12-03 13:34:01 -0500

    refactor: Adding Functions to Variables to Make Functions Universal

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,15 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+const validURL=  require('./js/nameChecker');
+const handleSubmit = require('./js/formHandler');
+import './styles/resets.scss'
+import './styles/header.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/base.scss'
+import './styles/messages.scss'
 
-console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export{
+	validURL,
+	handleSubmit
+}
+ 
