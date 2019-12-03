855dc0ee5ce32dc8f0f83cc7928067497ce19e5e
Thomas Gilmore <tomtitans@aol.com>
2019-12-03 13:37:09 -0500

    refactor: Changing Server Port in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,5 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const app = require('./app');
 
-const app = express()
-
-app.use(express.static('dist'))
-
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.listen(3000, function(){
+    console.log("We have started our server on port 3000");
+});
